ae7c3ba7e8b4ddd291f5bcfe3b58403411452a0a
Remco Blom <remco.blom@ordina.nl>
2020-03-26 17:16:35 +0100

    project init

---
+++ src/main/java/nl/ordina/poolautoapi/PoolautoApiApplication.java
@@ -0,0 +1,13 @@
+package nl.ordina.poolautoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoolautoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoolautoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/ordina/poolautoapi/PoolautoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.ordina.poolautoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoolautoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
