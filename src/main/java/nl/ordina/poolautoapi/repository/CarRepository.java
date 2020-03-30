1836d562baf4010ec80cad50d823daf91e745b23
Remco Blom <remco.blom@ordina.nl>
2020-03-30 17:27:14 +0200

    added all fields to Car and moved hardcoded data to Repository

--- src/main/java/nl/ordina/poolautoapi/repository/CarRepository.java
+++ src/main/java/nl/ordina/poolautoapi/repository/CarRepository.java
@@ -3,10 +3,28 @@ package nl.ordina.poolautoapi.repository;
 import nl.ordina.poolautoapi.model.Car;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+
 @Repository
 public class CarRepository {
 
     public Car getCar(String licensePlateNumber) {
-        return new Car(licensePlateNumber);
+        return new Car(
+                "22ZBR8",
+                "Skoda",
+                "Citigo",
+                "hatchback, 4 zitplaatsen",
+                "wit",
+                "FM5FM5CF0047MGVR2N1FA1SK",
+                new String[]{"airco","panoramadak","multimediasysteem"},
+                "3 cilinders, 999 inhoud",
+                "benzine",
+                125000,
+                LocalDate.of(2020, 10, 31),
+                450,
+                11779.00,
+                200,
+                LocalDate.of(2020,4,30)
+        );
     }
 }
