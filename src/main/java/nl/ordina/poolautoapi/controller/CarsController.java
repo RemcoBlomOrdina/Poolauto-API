1331a47fb2050f3e18bf62f3b5267ee7a6a3ccb9
Remco Blom <remco.blom@ordina.nl>
2020-04-20 15:12:30 +0200

    removed unused imports

--- src/main/java/nl/ordina/poolautoapi/controller/CarsController.java
+++ src/main/java/nl/ordina/poolautoapi/controller/CarsController.java
@@ -1,17 +1,12 @@
 package nl.ordina.poolautoapi.controller;
 
 import lombok.AllArgsConstructor;
-import nl.ordina.poolautoapi.externalapi.rdw.RDWCarDataFuel;
-import nl.ordina.poolautoapi.externalapi.rdw.RDWCarDataGeneral;
 import nl.ordina.poolautoapi.model.Car;
 import nl.ordina.poolautoapi.repository.CarRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/cars")
--- src/main/java/nl/ordina/poolautoapi/externalapi/rdw/RDWCarDataGeneral.java
+++ src/main/java/nl/ordina/poolautoapi/externalapi/rdw/RDWCarDataGeneral.java
@@ -1,11 +1,8 @@
 package nl.ordina.poolautoapi.externalapi.rdw;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.time.LocalDate;
 
 @Getter
 @Setter
