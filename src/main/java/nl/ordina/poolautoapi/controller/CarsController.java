f23b3fbf9f0e08008039a29a3a8ff03dd6cb0f03
Remco Blom <remco.blom@ordina.nl>
2020-04-28 14:02:17 +0200

    refactored licenseplatenumber from string to separate class

--- src/main/java/nl/ordina/poolautoapi/controller/CarsController.java
+++ src/main/java/nl/ordina/poolautoapi/controller/CarsController.java
@@ -2,15 +2,14 @@ package nl.ordina.poolautoapi.controller;
 
 import lombok.AllArgsConstructor;
 import nl.ordina.poolautoapi.model.Car;
+import nl.ordina.poolautoapi.model.LicensePlateNumber;
 import nl.ordina.poolautoapi.repository.CarRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/cars")
 @AllArgsConstructor
+@CrossOrigin
 public class CarsController {
 
     private final CarRepository carRepository;
@@ -18,6 +17,6 @@ public class CarsController {
     @GetMapping
     @RequestMapping("{licensePlateNumber}")
     public Car getCar(@PathVariable String licensePlateNumber) {
-        return carRepository.getCar(licensePlateNumber);
+        return carRepository.getCar(new LicensePlateNumber(licensePlateNumber));
     }
 }
--- src/main/java/nl/ordina/poolautoapi/repository/CarRepository.java
+++ src/main/java/nl/ordina/poolautoapi/repository/CarRepository.java
@@ -3,7 +3,9 @@ package nl.ordina.poolautoapi.repository;
 import nl.ordina.poolautoapi.externalapi.rdw.RDWCarDataFuel;
 import nl.ordina.poolautoapi.externalapi.rdw.RDWCarDataGeneral;
 import nl.ordina.poolautoapi.model.Car;
+import nl.ordina.poolautoapi.model.LicensePlateNumber;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Optional;
@@ -11,14 +13,28 @@ import java.util.Optional;
 @Repository
 public class CarRepository {
 
-    public Car getCar(String licensePlateNumber) {
+    public Car getCar(LicensePlateNumber licensePlateNumber) {
         RestTemplate restTemplate = new RestTemplate();
-        RDWCarDataGeneral[] rdwCarDataGeneralArray =
-                Optional.ofNullable(restTemplate.getForObject(RDWCarDataGeneral.API_URL + licensePlateNumber, RDWCarDataGeneral[].class))
-                        .orElse(new RDWCarDataGeneral[]{new RDWCarDataGeneral()});
-        RDWCarDataFuel[] rdwCarDataFuelArray =
-                Optional.ofNullable(restTemplate.getForObject(RDWCarDataFuel.API_URL + licensePlateNumber, RDWCarDataFuel[].class))
-                        .orElse(new RDWCarDataFuel[]{new RDWCarDataFuel()});
+
+        RDWCarDataGeneral[] rdwCarDataGeneralArray = null;
+        try {
+            rdwCarDataGeneralArray = Optional.ofNullable(restTemplate.getForObject(RDWCarDataGeneral.API_URL + licensePlateNumber, RDWCarDataGeneral[].class))
+                    .orElse(new RDWCarDataGeneral[]{new RDWCarDataGeneral()});
+        } catch (RestClientException e) {
+            System.out.println("RestClientException RDWCarDataGeneral");
+            rdwCarDataGeneralArray = new RDWCarDataGeneral[]{new RDWCarDataGeneral()};
+            e.printStackTrace();
+        }
+
+        RDWCarDataFuel[] rdwCarDataFuelArray = null;
+        try {
+            rdwCarDataFuelArray = Optional.ofNullable(restTemplate.getForObject(RDWCarDataFuel.API_URL + licensePlateNumber, RDWCarDataFuel[].class))
+                    .orElse(new RDWCarDataFuel[]{new RDWCarDataFuel()});
+        } catch (RestClientException e) {
+            System.out.println("RestClientException RDWCarDataFuel");
+            rdwCarDataFuelArray = new RDWCarDataFuel[]{new RDWCarDataFuel()};
+            e.printStackTrace();
+        }
 
         RDWCarDataGeneral rdwCarDataGeneral = rdwCarDataGeneralArray[0];
         RDWCarDataFuel rdwCarDataFuel = rdwCarDataFuelArray[0];
