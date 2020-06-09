d1ae481adaa42218bcbcc703f8eeb0ede0fef8b1
Remco Blom <remco.blom@ordina.nl>
2020-06-09 12:09:50 +0200

    load bijtelling properties from outside app, bijtelling returns null if exception occur instead of erroer string

--- src/main/java/nl/ordina/poolautoapi/model/Car.java
+++ src/main/java/nl/ordina/poolautoapi/model/Car.java
@@ -2,48 +2,63 @@ package nl.ordina.poolautoapi.model;
 
 import lombok.Getter;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.text.NumberFormat;
 import java.util.Locale;
-import java.util.NoSuchElementException;
-import java.util.ResourceBundle;
+import java.util.Optional;
+import java.util.Properties;
 
 @Getter
 public class Car {
 
-    private final String kenteken;          // kenteken
-    private final String merk;                       // merk
-    private final String model;                       // model
-    private final String type;                        // type
-    private final String kleur;                       // kleur
-    private final String uitvoering;                     // uitvoering
+    private static final String PATH_BIJTELLING_PROPERTIES = System.getProperty("user.home")
+            + "/ApplicationProperties/poolauto-api/bijtelling.properties";
+
+    private String kenteken;          // kenteken
+    private String merk;                       // merk
+    private String model;                       // model
+    private String type;                        // type
+    private String kleur;                       // kleur
+    private String uitvoering;                     // uitvoering
 //    private final String[] accessoires;               // accessoires
-    private final String motor;                      // motor
-    private final String brandstof;                        // brandstof
-//    private final String milieulabel;
+//    private String motor;                      // motor
+    private String aantal_cilinders;
+    private String cilinderinhoud;
+    private String brandstof;                        // brandstof
+    private String milieulabel;
 //    private final int kilometrage;                   // kilometrage
 //    private final LocalDate resterendeContractuur;          // resterende contractduur
 //    private final double leasebedrag;                 // leasebedrag
-    private final String cataloguswaarde;                // cataloguswaarde
-    private final String bijtellingsPercentage;
-    private final String brutoBijtellingPerMaand;                    // bijtelling
+    private String cataloguswaarde;                // cataloguswaarde
+    private String datum_eerste_afgifte;
+    private String bijtellingspercentage;
+    private String bruto_bijtelling_per_maand;                    // bijtelling
 //    private final LocalDate uitersteOvernamedatum;    // uiterste overnamedatum
 //    private List<byte[]> fotos;              // foto's
 //    private List<String> eventueleSchade;               // eventuele schade
 
+    private static final String ERROR_MESSAGE = "berekening niet mogelijk";
+    private static final NumberFormat dutchCurrencyFormat = NumberFormat.getCurrencyInstance(new Locale("nl", "NL"));
 
-    public Car(String kenteken, String merk, String model, String type, String kleur, String uitvoering, String motor,
-               String brandstof, String cataloguswaarde, String datumEersteAfgifteNederland, String co2UitstootGecombineerd) {
+    public Car(String kenteken, String merk, String model, String type, String kleur, String uitvoering, String aantalCilinders, String cilinderInhoud,
+               String brandstof, String milieulabel, String cataloguswaarde, String datumEersteAfgifteNederland, String co2UitstootGecombineerd) {
         this.kenteken = kenteken;
         this.merk = merk;
         this.model = model;
         this.type = type;
-        this.kleur = kleur;
+        this.kleur = kleur.toLowerCase();
         this.uitvoering = uitvoering;
-        this.motor = motor;
-        this.brandstof = brandstof;
+        this.aantal_cilinders = aantalCilinders;
+        this.cilinderinhoud = cilinderInhoud;
+        this.brandstof = brandstof.toLowerCase();
+        this.milieulabel = milieulabel;
         this.cataloguswaarde = cataloguswaarde;
-        this.bijtellingsPercentage = calculateBijtellingsPercentage(datumEersteAfgifteNederland, co2UitstootGecombineerd, brandstof, cataloguswaarde);
-        this.brutoBijtellingPerMaand = calculateBrutoBijtellingPerMaand(cataloguswaarde, bijtellingsPercentage);
+        this.datum_eerste_afgifte = datumEersteAfgifteNederland;
+        this.bijtellingspercentage = calculateBijtellingsPercentage(datumEersteAfgifteNederland, co2UitstootGecombineerd, brandstof, cataloguswaarde);
+        this.bruto_bijtelling_per_maand = calculateBrutoBijtellingPerMaand(cataloguswaarde, bijtellingspercentage);
     }
 
     private String calculateBrutoBijtellingPerMaand(String cataloguswaarde, String bijtellingsPercentage) {
@@ -53,23 +68,26 @@ public class Car {
             cataloguswaardeInt = Integer.parseInt(cataloguswaarde);
             bijtellingsPercentageInt = Integer.parseInt(bijtellingsPercentage);
         } catch (NumberFormatException e) {
-            return "onbekend";
+            return null;
         }
 
-        NumberFormat numberFormat = NumberFormat.getCurrencyInstance(new Locale("nl", "NL"));
-        return numberFormat.format(cataloguswaardeInt * ((double)bijtellingsPercentageInt / 100) / 12);
+        DecimalFormat decimalFormat = new DecimalFormat("####0.00", DecimalFormatSymbols.getInstance(Locale.US));
+        return String.valueOf(decimalFormat.format(cataloguswaardeInt * ((double)bijtellingsPercentageInt / 100) / 12));
     }
 
     private String calculateBijtellingsPercentage(String datumEersteAfgifteNederland, String co2UitstootGecombineerd, String brandstof, String cataloguswaarde) {
+        String result;
         try {
             String jaarEersteAfgifteNederland = datumEersteAfgifteNederland.substring(0,4);
             String brandstofLowerCase = brandstof.toLowerCase();
             int co2UitstootGecombineerdInt = Integer.parseInt(co2UitstootGecombineerd);
             int cataloguswaardeInt = Integer.parseInt(cataloguswaarde);
-            ResourceBundle resourceBundle = ResourceBundle.getBundle("bijtelling");
 
-            return resourceBundle.keySet().stream()
-                    .map(k -> new BijtellingProperty(k, resourceBundle.getString(k)))
+            Properties properties = new Properties();
+            properties.load(new FileInputStream(PATH_BIJTELLING_PROPERTIES));
+
+            Optional<BijtellingProperty> optional = properties.keySet().stream()
+                    .map(k -> new BijtellingProperty(k.toString(), properties.getProperty(k.toString())))
                     .filter(b -> b.jaar.equals(jaarEersteAfgifteNederland))
                     .filter(b -> b.brandstof.contains(brandstofLowerCase))
                     .filter(b -> {
@@ -79,10 +97,12 @@ public class Car {
                             return b.minimumwaarde <= co2UitstootGecombineerdInt && b.maximumwaarde >= co2UitstootGecombineerdInt;
                         }
                     })
-                    .findAny().get().bijtellingspercentage;
-        } catch (NumberFormatException | NoSuchElementException e) {
-            return "berekening niet mogelijk";
+                    .findAny();
+            result = optional.map(bijtellingProperty -> bijtellingProperty.bijtellingspercentage).orElse(null);
+        } catch (NumberFormatException | IOException e) {
+            result = null;
         }
+        return result;
     }
 
     private static class BijtellingProperty {
