7fcf1e0512229f5b6468926ddf5593df082fa807
Remco Blom <remco.blom@ordina.nl>
2020-06-09 11:57:15 +0200

    added boolean error

--- src/main/java/nl/ordina/poolautoapi/exception/APIError.java
+++ src/main/java/nl/ordina/poolautoapi/exception/APIError.java
@@ -5,11 +5,13 @@ import lombok.Getter;
 @Getter
 public class APIError {
 
+    private final boolean error;
     private final int httpStatusCode;
     private final String httpStatusBeschrijving;
     private final String oorzaak;
 
     public APIError(PoolautoException e) {
+        this.error = true;
         this.httpStatusCode = e.getHttpStatusCode();
         this.httpStatusBeschrijving = e.getHttpStatusBeschrijving();
         this.oorzaak = e.getOorzaak();
